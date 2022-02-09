4401e1591d8d90420d4307b7769a588ef64f702b
Alexander <154324-alexander-rykov@users.noreply.gitlab.com>
2022-02-09 16:32:20 +0300

    Add solution configuration files: editor configs, stylecop settings, style ruleset, readme. Add projects with unit tests.

---
+++ SpiralMatrix/MatrixExtension.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SpiralMatrix
+{
+    /// <summary>
+    /// Matrix manipulation.
+    /// </summary>
+    public static class MatrixExtension
+    {
+        /// <summary>
+        /// Fills the matrix with natural numbers, starting from 1 in the top-left corner, increasing in an inward, clockwise spiral order.
+        /// </summary>
+        /// <param name="size">Matrix size.</param>
+        /// <returns>Filled matrix.</returns>
+        /// <exception cref="ArgumentException">Thrown when matrix size less or equal zero.</exception>
+        /// <example> size = 3
+        ///     1 2 3
+        ///     8 9 4
+        ///     7 6 5.
+        /// </example>
+        /// <example> size = 4
+        ///     1  2  3  4
+        ///     12 13 14 5
+        ///     11 16 15 6
+        ///     10 9  8  7.
+        /// </example>
+        public static int[,] GetMatrix(int size)
+        {
+            throw new NotImplementedException("You need to implement this method.");
+        }
+    }
+}
